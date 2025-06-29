39da1c3a66fa16d306ae046305dd52a7944ced73
pavanivudatha <vudathapavani@gmail.com>
2025-06-29 14:12:39 +0530

    render url commmit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ai-text-to-image-generator-9jd4.onrender.com",
 });
 
 export const GetPosts = async () => await API.get("/post/");
